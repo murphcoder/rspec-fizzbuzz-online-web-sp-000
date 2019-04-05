fd22b3759cae3ba1a8dd3512af07f9c48efaede2
Sean Murphy <filmmakersean@fastmail.net>
2019-04-05 00:18:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 15 == 0
+    puts "fizzbuzz"
+  elsif int % 5 == 0
+    puts "buzz"
+  end
+  elsif int % 3 == 0
+    puts "fizz"
+  end
+end
