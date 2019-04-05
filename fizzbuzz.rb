e4082fe7ffb6ec9e9f834aadd61f8161506638e1
Sean Murphy <filmmakersean@fastmail.net>
2019-04-05 00:24:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 15 == 0
-    "Fizzbuzz"
+    return "Fizzbuzz"
   elsif int % 5 == 0
-    "Buzz"
+    return "Buzz"
   elsif int % 3 == 0
-    "Fizz"
+    return "Fizz"
   else
-    nil
+    return nil
   end
 end
-num = gets.strip
-puts fizzbuzz(num)
